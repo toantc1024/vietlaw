66f3eff547c2a097cf8db76ef8a0dda5983d488d
Tran Cong Toan <22110248@student.hcmute.edu.vn>
2023-12-04 08:48:29 +0700

    Models: set up model

---
+++ packages/backend/database/model_setup.py
@@ -0,0 +1,42 @@
+
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+import torch
+
+MODEL_FOLDER = "nhantran0506/law-llms-v2"
+TOKENIZER_FOLDER = "nhantran0506/law-llms-v2"
+
+model = T5ForConditionalGeneration.from_pretrained(MODEL_FOLDER)
+tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_FOLDER)
+
+
+# Document retrival
+doc = ""
+
+
+# Grenerate question
+
+question = input("Question: ")
+
+prompt = f"""
+        {doc}
+        {question}
+        
+        
+       
+        
+        """
+
+DEVICE = "cpu" if not torch.cuda.is_available() else "cuda"
+
+input_ids = tokenizer.encode(
+    prompt, return_tensors="pt", max_length=512, truncation=True).to(DEVICE)
+
+output = model.generate(input_ids,
+                        max_length=128,
+                        num_beams=2,
+                        early_stopping=True,
+                        top_p=0.95,
+                        top_k=30,
+                        do_sample=True)
+
+generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
--- packages/backend/database/vector_embedded.py
+++ packages/backend/database/vector_embedded.py
@@ -1,13 +1,23 @@
 import weaviate
-from weaviate.embedded import EmbeddedOptions
+import weaviate.classes as wvc
 
-client = weaviate.Client(
-    embedded_options=EmbeddedOptions()
+# As of November 2023, we are working towards making all WCS instances compatible with the new API introduced in the v4 Python client.
+# Accordingly, we show you how to connect to a local instance of Weaviate.
+# Here, authentication is switched off, which is why you do not need to provide the Weaviate API key.
+client = weaviate.connect_to_local(
+    port=8080,
+    grpc_port=50051,
+    headers={
+        # Replace with your inference API key
+        "X-OpenAI-Api-Key": os.environ["OPENAI_APIKEY"]
+    }
 )
 
-data_obj = {
-    "name": "Chardonnay",
-    "description": "Goes with fish"
-}
+questions = client.collections.get("Question")
 
-client.data_object.create(data_obj, "Wine")
+response = questions.query.near_text(
+    query="biology",
+    limit=2
+)
+
+print(response.objects[0].properties)  # Inspect the first object
