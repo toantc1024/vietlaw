991e4e651fa9a3672a361bb94421aa9d45dc488a
Toan Tran Cong <60417892+toantc1024@users.noreply.github.com>
2023-12-07 13:52:41 +0700

    Update main.py

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -1,7 +1,3 @@
-from transformers import T5ForConditionalGeneration, AutoTokenizer
-import torch
-import requests
-import uvicorn
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from auth import *
@@ -26,12 +22,6 @@ deMuc = db.get_collection(name='deMuc')
 
 MODEL_FOLDER = "nhantran0506/law-llms-v1"
 TOKENIZER_FOLDER = "nhantran0506/law-llms-v1"
-
-model = T5ForConditionalGeneration.from_pretrained(MODEL_FOLDER)
-tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_FOLDER)
-
-
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
 
 client = Client("https://nhantran0506-vietlaw-llms.hf.space/--replicas/tr6ht/")
@@ -104,51 +94,10 @@ async def fake_video_streamer():
 
 @app.post('/api/chatbot', dependencies=[Depends(validate_token)])
 def test(request: Chatbot):
-    # Document retrival
-    doc = deMuc.query(
-        query_texts=[request.query],
-        n_results=1
+    result = client.predict(
+        request.text,  # str  in 'Input' Textbox component
+        api_name="/predict"
     )
-
-    if doc[1]["distance"] < 40:
-        return {
-            'result': doc["documents"][0]
-        }
-
-    # Grenerate question
-
-    question = request.text
-
-    prompt = f"""
-            {doc}
-            {question}
-            """
-
-    DEVICE = "cpu" if not torch.cuda.is_available() else "cuda"
-
-    input_ids = tokenizer.encode(
-        prompt, return_tensors="pt", max_length=512, truncation=True).to(DEVICE)
-
-    output = model.generate(input_ids,
-                            max_length=1024,
-                            num_beams=2,
-                            early_stopping=True,
-                            temperature=0.3,
-                            top_k=30,
-                            do_sample=True)
-
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-    doc = deMuc.query(
-        query_texts=[generated_text],
-        n_results=2
-    )
-
-    if doc[1]["distance"] > 70:
-        return {
-            'result': f"Xin lỗi, tôi không hiểu câu hỏi '{question}'. Bạn có thể đặt câu hỏi khác được không?"
-        }
-
     return {
-        'result': output
+        result: result
     }
